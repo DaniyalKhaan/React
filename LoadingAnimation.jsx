84a5a9192d2049d08665ee6add40026275af637a
Daniyal Khan <124635608+DaniyalKhaan@users.noreply.github.com>
2024-04-25 01:13:15 +0500

    Loading Circle

--- LoadingAnimation.jsx
+++ LoadingAnimation.jsx
@@ -8,3 +8,30 @@ function LoadingAnimation() {
 }
 
 export default LoadingAnimation;
+
+
+// CSS 
+
+// .Circle{
+//   width: 20px;
+//   height: 20px;
+//   /* background-color: rgba(27, 28, 26, 0.357); */
+//   margin-top: 40px;
+//   border: 2px solid rgb(255, 255, 255);
+//   border-top-color: rgb(0, 0, 0);
+//   border-radius: 50%;
+//   text-align: center;
+//   animation: loading 1s linear infinite;
+// }
+
+// @keyframes loading{
+//   0%{
+//     transform: rotate(0deg);
+
+//   }
+
+//   100%{
+//     transform: rotate(360deg);
+
+//   }
+// }
